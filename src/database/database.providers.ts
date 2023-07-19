d4bedf87fb5819d9f933280ebec2135d002724d6
DℰℕVℰℛ <barroromario02@gmail.com>
2023-07-19 15:00:33 +0000

    Update database.providers.ts

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://localhost:27017/xarala-test'),
+      mongoose.connect('mongodb+srv://kronance:kronancetest@denver.n3zfpjj.mongodb.net/xarala?serverSelectionTimeoutMS=20000&authSource=admin'),
   },
 ];
